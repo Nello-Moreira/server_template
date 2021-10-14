4f0fb71bad69e3540b077293a1dfe7c850affdf5
Nello-Moreira <g.moreira1@outlook.com>
2021-10-14 09:33:06 -0300

    added postgres connection

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import pg from 'pg';
 
 import exampleRoute from './src/routes/example-route.js';
 
 const port = 4000;
 const server = express();
-
 server.use(cors());
+server.use(express.json());
+
+const { Pool } = pg;
+const config = {
+	port: 5432,
+	user: 'postgres',
+	password: '123456',
+	host: 'localhost',
+	database: 'example_data',
+};
+const connection = new Pool(config);
 
 server.get(exampleRoute.route, exampleRoute.getRoute);
 
