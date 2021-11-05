b57c812c1c1aa960177c85d042db3b5becffa319
Nello-Moreira <g.moreira1@outlook.com>
2021-11-05 18:13:12 -0300

    changed connection file to receive heroku environment variables

--- src/data/connection.js
+++ src/data/connection.js
@@ -2,20 +2,24 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-let dbConnection;
-
-try {
-	const config = {
+function createConfig() {
+	if (process.env.NODE_ENV === 'production') {
+		return {
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+				rejectUnauthorized: false,
+			},
+		};
+	}
+	return {
 		port: process.env.DB_PORT,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		host: process.env.DB_HOST,
 		database: process.env.DB_DATABASE,
 	};
-
-	dbConnection = new Pool(config);
-} catch (error) {
-	console.log(error);
 }
+
+const dbConnection = new Pool(createConfig());
 
 export default dbConnection;
