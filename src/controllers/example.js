6c7783649b374fac5447ff4a777fc27578d5444e
Nello-Moreira <g.moreira1@outlook.com>
2021-12-01 21:46:24 -0300

    updated: layer architecture structure && implemented: express router and middleware for database errors

--- src/controllers/example.js
+++ src/controllers/example.js
@@ -3,6 +3,7 @@ import exampleError from '../errors/example.js';
 
 async function getRoute(request, response, next) {
 	try {
+		// eslint-disable-next-line no-unused-vars
 		const examples = await exampleService.searchExamples();
 
 		return response.status(501).send('this is an example route');
--- src/middlewares/databaseError.js
+++ src/middlewares/databaseError.js
@@ -3,6 +3,7 @@ export default function databaseErrorMiddleware(
 	error,
 	request,
 	response,
+	// eslint-disable-next-line no-unused-vars
 	next
 ) {
 	console.error(error);
